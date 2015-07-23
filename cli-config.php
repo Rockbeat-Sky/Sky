9a98a433662ce4aefd04373b2ef32a586d04846e
Wong Hansen <huanghanzen@gmail.com>
2015-07-23 21:57:38 +0700

    add folder cache and log

--- cli-config.php
+++ cli-config.php
@@ -1,4 +1,22 @@
 <?php
+ /**
+ * Sky Framework
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package		Sky Framework
+ * @author		Hansen Wong
+ * @copyright	Copyright (c) 2015, Rockbeat.
+ * @license		http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link		http://rockbeat.web.id
+ * @since		Version 1.0
+ */
+ 
+/**
+This is CLI CONFIG for Doctrine 
+**/
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Sky\doctrine\ORM;
 
